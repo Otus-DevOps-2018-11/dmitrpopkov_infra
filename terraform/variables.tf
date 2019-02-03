dcb515fc2274bd1d8d8349bd6d9272442ac13d87
Dmitry Popkov <dmitrpopkov@gmail.com>
2019-02-03 06:12:30 -0500

    Terraform Format files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,24 @@
 variable project {
- description = "Project ID"
+  description = "Project ID"
 }
+
 variable region {
- description = "Region"
- default = "europe-west1"
+  description = "Region"
+  default     = "europe-west1"
 }
+
 variable public_key_path {
- description = "Path to the public key used for ssh access"
+  description = "Path to the public key used for ssh access"
 }
+
 variable disk_image {
- description = "Disk image"
+  description = "Disk image"
 }
+
 variable private_key_path {
- description = "Path to the rivate key used for ssh access"
+  description = "Path to the rivate key used for ssh access"
 }
+
 variable zone {
   description = "Zone"
   default     = "europe-west1-d"
